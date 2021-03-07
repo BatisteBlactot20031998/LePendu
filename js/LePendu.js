8a3349dd671a184d6bdc399835051c39cdacbd67
BatisteBlactot20031998 <batiste.blactot.boulot@gmail.com>
2021-03-07 13:03:08 +0100

    add code introduction for my game, creat 2 let and 1 const for my words

--- js/LePendu.js
+++ js/LePendu.js
@@ -3,3 +3,19 @@ alert("Bon.. comment dire... ");
 alert("Lors de notre dernière rencontre tu as tout simplement écrasé notre champion sur le jeu du Chifoumi..");
 alert("Mais ne crois pas que tu es le plus fort loin de là !!");
 alert("Nous allons dès maintenant te proposer un défi contre notre champion du Pendu et tu ne t'en sortiras pas aussi facilement");
+
+let lifepoint = 7; 
+let restOfMyWord = 0;
+const MySecretWord = [
+    "heineken",
+    "grimbergen",
+    "1664",
+    "affligem",
+    "pelforth",
+    "goudale",
+    "desperados",
+    "leffe",
+    "kronenbourg",
+    "superbock"
+];
+
