63dc4f1054916345874f07d6fa1dc678e7942f13
BatisteBlactot20031998 <batiste.blactot.boulot@gmail.com>
2021-03-07 12:56:53 +0100

    add alert for presentation of the game Le Pendu

---
+++ js/LePendu.js
@@ -0,0 +1,5 @@
+alert("Salut à toi Aventurier !");
+alert("Bon.. comment dire... ");
+alert("Lors de notre dernière rencontre tu as tout simplement écrasé notre champion sur le jeu du Chifoumi..");
+alert("Mais ne crois pas que tu es le plus fort loin de là !!");
+alert("Nous allons dès maintenant te proposer un défi contre notre champion du Pendu et tu ne t'en sortiras pas aussi facilement");
