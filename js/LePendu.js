61c8506b831d8a70ccf31f9ebd353a0d4095181f
BatisteBlactot20031998 <batiste.blactot.boulot@gmail.com>
2021-03-07 13:12:23 +0100

    add function randomword, split in array, convert a word at underscore and anticheat

--- js/LePendu.js
+++ js/LePendu.js
@@ -19,3 +19,27 @@ const MySecretWord = [
     "superbock"
 ];
 
+function choiceRandomWord(){
+    let randomWordInMyConst = Math.floor(Math.random() * Math.floor(MySecretWord.length));
+    return MySecretWord[randomWordInMyConst];
+};
+
+function splitRandomWordInArray(randomWordInMyConst) {
+    let arrayWord = randomWordInMyConst.split("");
+    return arrayWord;
+};
+
+function showHiddenLetter (randomWordInMyConst) {
+    let hiddenLetter = [];
+    for (let i =0; i < randomWordInMyConst.length; i++){
+        hiddenLetter[i] = " _ ";
+    }
+    return hiddenLetter;
+};
+
+function noCheatOneLetter (oneLetter){
+    while(oneLetter.length > 1) {
+        oneletter = prompt("Bah oui bien sûr !! C'est pas parce que je suis un Développeur Web Junior que tu dois commencer à tricher...Choisis une lettre merci !");
+    }
+    return oneLetter
+};
